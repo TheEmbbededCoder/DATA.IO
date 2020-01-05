98950b3d391dcf858b5f72581c2f3ca08fe0d5f3
francisco <francisco.mendes@eyeware.tech>
2020-01-05 19:48:59 +0000

    inicio do site

---
+++ webapp.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import render_template
+from flask import request, url_for, redirect
+from flask import jsonify
+import requests
+import json
+import io
+
+import random
+import string
+
+import datetime 
+
+app = Flask(__name__)
+
+
+# # ERROR resource not found page
+# @app.errorhandler(404)
+# def page_not_found(e):
+# 	return render_template('serviceOfflineTemplate.html', type="found", services = microservices, login = user, key = key)
+
+
+@app.route('/favicon.ico')
+def favicon():
+	return redirect(url_for('static', filename='favicon.ico'), code=302)
+
+############ HTML #############
+@app.route('/')
+def homePage():
+	return render_template("homepage.html")
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+	if request.method == 'POST':
+		if "user" in request.form:
+			user = request.form["user"]
+			return render_template("login.html", user = user)
+	return redirect(url_for('homePage'), code=302)
+
+@app.route('/index')
+def index():
+	return render_template("index2.html")
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
